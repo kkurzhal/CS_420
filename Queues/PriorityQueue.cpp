70625b893fab38df4ed0a9cabe83fbf83b7dea1d
Kyle Kurzhal <kkurzhal@gmail.com>
2014-10-05 23:45:03 -0500

    Commented out what seems unnecessary, and added a few items.

--- Queues/PriorityQueue.cpp
+++ Queues/PriorityQueue.cpp
@@ -1,7 +1,7 @@
 #include "PriorityQueue.h"
 
 template<typedef T>
-PriorityQueue<T>::PriorityQueue(int size)  {
+PriorityQueue<T>::PriorityQueue(int size = 20)  { //added a default to act as default constructor
 		SIZE = size;
 		priorities = new T[SIZE];
 		count = 0;
@@ -9,7 +9,7 @@ PriorityQueue<T>::PriorityQueue(int size)  {
 
 template<typedef T>
 PriorityQueue<T>::~PriorityQueue()  {
-	delete [] priorities;
+	delete [] priorities;	//do we need to erase all data before deleting the array?
 	priorities = NULL;
 }
 
@@ -29,12 +29,18 @@ bool PriorityQueue<T>::isEmpty()  {
 	if(count == 0)  {
 		return true;
 	}
+	else {
+		return false;
+	}
+/*		//I removed the following lines since the count should determine if it is empty or not.
+
 	for(int i = 0; i < SIZE; i++)  {
 		if(!priorities[i].isEmpty())  {
 			return false;
 		}
 	}
 	return true;
+*/
 }
 
 template<typedef T>
@@ -43,17 +49,22 @@ int PriorityQueue<T>::pop()  {
 		cout << "Cannot pop item from empty queue!" << endl;
 	}
 	else  {
+
+		priorities[count-1] = NULL;
+/*	//Taken out because it implements a stack, not a FIFO queue
+
 		for(int i = 0; i < SIZE; i++)  {
 			priorities[i] = priorities[i+1];
 		}
+*/
 		count--;
 	}
 }
 
 template<typedef T>
 T PriorityQueue<T>::fetch(int index)  {
-	
-} 
+	return priorities[index];	
+}
 
 
 
